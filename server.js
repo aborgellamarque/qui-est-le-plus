dbf075ed8987c3bc29aa5d3976688ecfc891ff62
aborgellamarque <antoninborgellamarque@gmail.com>
2025-12-29 21:24:24 +0100

    Fix Render : force polling for Socket.IO

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  transports: ["polling"]
+});
 
 app.use(express.static("public"));
 
